d29dd21e23827b1da531dfc365466590a9bee747
dan-codes <dli123@syr.edu>
2019-06-27 01:29:48 -0400

    location button

--- app/src/main/java/com/example/cribb/MapsActivity.kt
+++ app/src/main/java/com/example/cribb/MapsActivity.kt
@@ -22,21 +22,42 @@ import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse
 import com.google.android.gms.tasks.Task
 import android.content.pm.PackageManager
 import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
+import android.location.Location
 import android.util.Log
+import android.view.View
+import android.widget.RelativeLayout
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
+import com.google.android.libraries.places.api.model.AutocompletePrediction
 import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest
+import com.google.android.libraries.places.widget.AutocompleteActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.GeoPoint
+import com.mancj.materialsearchbar.MaterialSearchBar
 import java.util.Arrays.asList
 
 
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
-    val db = FirebaseFirestore.getInstance()
+    private val db = FirebaseFirestore.getInstance()
     private lateinit var mMap: GoogleMap
+    private lateinit var mFusedLocationProviderClient : FusedLocationProviderClient
+    private lateinit var placesClient: PlacesClient
 
+    private lateinit var predictionList: List<AutocompletePrediction>
+    private lateinit var mLastKnownLocation : Location
+    private lateinit var locationCallback : LocationCallback
+    private lateinit var mapView : View
+    private lateinit var materialSearchBar:MaterialSearchBar
 
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
@@ -45,10 +66,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        materialSearchBar = findViewById(R.id.searchBar)
+
         // Initialize Places.
         Places.initialize(applicationContext, "AIzaSyDoTyyUjp3Xtdll-X3-zTnORb66-fkCUNE")
 
 // Create a new Places client instance.
+        mapView = mapFragment.view!!
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
 
     }
@@ -62,6 +87,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
 
         mMap = googleMap
@@ -84,10 +110,40 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Add a marker in Sydney and move the camera
         val syracuse = LatLng(43.038710, -76.134265)
         //mMap.addMarker(MarkerOptions().position(syracuse).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(syracuse))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(syracuse, 12.0f))
+        setUpMap() //checks if location is enabled and requests users permission
+        mMap.isMyLocationEnabled = true //creates a blue location dot and location button
+
+        //moves location button to bottom right
+        val locationButton= (mapView.findViewById<View>(Integer.parseInt("1")).parent as View).findViewById<View>(Integer.parseInt("2"))
+        val rlp=locationButton.layoutParams as (RelativeLayout.LayoutParams)
+        // position on right bottom
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP,0)
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM,RelativeLayout.TRUE)
+        rlp.setMargins(0,0,30,30)
+
+        mFusedLocationProviderClient.lastLocation.addOnSuccessListener(this) { location ->
+            // Got last known location. In some rare situations this can be null.
+            // 3
+            if (location != null) {
+                mLastKnownLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
+
     }
 
     fun findLocation(){
+    }
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+
+            return
+        }
     }
 
 }
