45f6923f9342a93cff8c2a971fcab60eedfa2252
dan-codes <dli123@syr.edu>
2019-07-02 01:20:17 -0400

    updated google maps fragment

--- app/src/main/java/com/example/cribb/MapFragment.kt
+++ app/src/main/java/com/example/cribb/MapFragment.kt
@@ -1,12 +1,29 @@
 package com.example.cribb
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.model.AutocompletePrediction
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.GeoPoint
+import com.mancj.materialsearchbar.MaterialSearchBar
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -23,18 +40,32 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  *
  */
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
    // private var listener: OnFragmentInteractionListener? = null
 
+    private val db = FirebaseFirestore.getInstance()
+    private lateinit var mMap: GoogleMap
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var placesClient: PlacesClient
+
+    private lateinit var predictionList: List<AutocompletePrediction>
+    private lateinit var mLastKnownLocation: Location
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var mapView: View
+    private lateinit var mMapView: MapView
+    private lateinit var materialSearchBar: MaterialSearchBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
     }
 
     override fun onCreateView(
@@ -42,7 +73,89 @@ class MapFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        mapView = inflater.inflate(R.layout.fragment_map, container, false)
+
+//        mMapView = ((mapView.findViewById(R.id.maps)))
+//        mMapView.onCreate(null)
+//        mMapView.onResume()
+//        mMapView.getMapAsync(this)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+        return mapView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onMapReady(googleMap: GoogleMap) {
+
+        mMap = googleMap
+
+        db.collection("listings")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("TAG", "${document.id} => ${document.data}")
+                    val location: GeoPoint? = document.getGeoPoint("geopoint")
+                    val address: String? = document.get("address").toString()
+                    mMap.addMarker(
+                        MarkerOptions().position(LatLng(location!!.latitude, location.longitude)).title(
+                            address
+                        )
+                    )
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d("TAG", "Error getting documents: ", exception)
+            }
+
+
+//        // Add a marker in Sydney and move the camera
+//        val syracuse = LatLng(43.038710, -76.134265)
+//        //mMap.addMarker(MarkerOptions().position(syracuse).title("Marker in Sydney"))
+//        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(syracuse, 12.0f))
+        setUpMap() //checks if location is enabled and requests users permission
+        mMap.isMyLocationEnabled = true //creates a blue location dot and location button
+//
+//        //moves location button to bottom right
+        val locationButton =
+            (mapView.findViewById<View>(Integer.parseInt("1")).parent as View).findViewById<View>(Integer.parseInt("2"))
+        val rlp = locationButton.layoutParams as (RelativeLayout.LayoutParams)
+        // position on right bottom
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0)
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
+        rlp.setMargins(0, 0, 30, 150)
+//        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+//        mFusedLocationProviderClient.lastLocation.addOnSuccessListener { location: Location? ->
+//            // Got last known location. In some rare situations this can be null.
+//            // 3
+//            if (location != null) {
+//                mLastKnownLocation = location
+//                val currentLatLng = LatLng(mLastKnownLocation.latitude, mLastKnownLocation.longitude)
+//                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+//            }
+//        }
+
+//        mMap.setOnInfoWindowClickListener(this)
+
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(
+                context!!,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                activity!!,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE
+            )
+
+            return
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -99,4 +212,8 @@ class MapFragment : Fragment() {
 //                }
 //            }
 //    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 }
