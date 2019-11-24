dcd7012b1c9d8a49d376c127b140d13273fafea6
Leo Chen <schen23@syr.edu>
2019-11-23 19:35:50 -0500

    wirte reviews

--- app/src/main/java/com/example/cribb/writeReviewFragment.kt
+++ app/src/main/java/com/example/cribb/writeReviewFragment.kt
@@ -2,19 +2,24 @@ package com.example.cribb
 
 
 import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import android.widget.Toast
+import androidx.navigation.Navigation
 import com.example.cribb.ui.login.LoginResult
 import com.example.cribb.ui.login.LoginViewModel
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import java.security.AuthProvider
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FieldValue
+import com.google.firebase.firestore.SetOptions
 import kotlinx.android.synthetic.main.fragment_create_listing.view.*
 import kotlinx.android.synthetic.main.fragment_display_listing.*
 import kotlinx.android.synthetic.main.fragment_write_review.*
@@ -31,7 +36,7 @@ private const val ARG_PARAM2 = "param2"
  */
 class writeReviewFragment : Fragment() {
 
-
+    private var edited:Boolean = false
     private lateinit var address:String
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -56,6 +61,181 @@ class writeReviewFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         address_review.text = address
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                overall_rating.text = seekBar!!.progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar) {
+                // Do something
+                Toast.makeText(context!!, "Start", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar) {
+                // Do something
+                Toast.makeText(context!!, "End", Toast.LENGTH_SHORT).show()
+            }
+        })
+
+        button_post_review.setOnClickListener{
+            val user = FirebaseAuth.getInstance().currentUser
+            val addressRef = db.collection("listings").document(address)
+            val userRef = db.collection("Users").document(user!!.email!!)
+
+            val comment = review_comments.text ?: "No Comment!"
+
+            if (edited) {
+                var propertyFields = hashMapOf<Any, Any>()
+                val userFields = hashMapOf<Any, Any>()
+                val reviewer = hashMapOf<Any, Any>()
+                val property = hashMapOf<Any, Any>()
+
+                val reviewDetail = hashMapOf<Any,Any>(
+                    "comments" to comment.toString(),
+                    "isAnonymous" to anonymous_switch.isChecked,
+                    "isEdited" to true,
+                    "rating" to overall_rating.text.toString().toFloat(),
+                    "willLiveAgain" to live_here_again_switch.isChecked,
+                    "locationRating" to location_ratingBar.rating,
+                    "managementRating" to manage_ratingBar.rating,
+                    "amenitiesRating" to amenities_ratingBar.rating,
+                    "timeStamp" to FieldValue.serverTimestamp()
+                )
+
+                propertyFields["reviews"] = reviewer
+                reviewer[user!!.email!!] = reviewDetail
+
+                userFields["Review History"] = property
+                property[address] = reviewDetail
+
+
+                var avgAmR = 0.0
+                var avgMnR = 0.0
+                var avgLcR = 0.0
+                var avgOAR = 0.0
+                addressRef.get().addOnSuccessListener { document ->
+                        if (document != null) {
+                            var totalOverallRating = 0.0
+                            var totalAmentRating = 0.0
+                            var totalLocRating = 0.0
+                            var totalManageRating = 0.0
+
+                            var reviewerNum = 0
+
+                            val review = document.get("reviews") as HashMap<String, *>
+
+                            for (reviewer in review) {
+                                reviewerNum++
+                                val reviewMap = review
+                                val reviewInfo:HashMap<String,*>  = reviewMap.getValue(reviewer.key) as HashMap<String, *>
+                                totalLocRating += (reviewInfo.getValue("locationRating")).toString().toFloat()
+                                totalAmentRating += (reviewInfo.getValue("amenitiesRating")).toString().toFloat()
+                                totalManageRating += (reviewInfo.getValue("managementRating")).toString().toFloat()
+
+                                totalOverallRating += (reviewInfo.getValue("rating")).toString().toFloat()
+                            }
+                            avgAmR = totalAmentRating/reviewerNum
+                            avgMnR = totalManageRating/reviewerNum
+                            avgLcR = totalLocRating/reviewerNum
+
+                            avgOAR = totalOverallRating/reviewerNum
+                        }
+                    }
+
+                propertyFields = hashMapOf(
+                    "avgAmenities" to avgAmR,
+                    "avgLocation" to avgLcR,
+                    "avgManage" to avgMnR,
+                    "avgOverallRating" to avgOAR
+                )
+
+                addressRef.set(propertyFields, SetOptions.merge()).addOnSuccessListener {
+                    Log.d(TAG, "DocumentSnapshot successfully written!")
+                    var nextAction = writeReviewFragmentDirections.actionWriteReviewFragment2ToDisplayListingFragment2()
+                    nextAction.dynamicAddress = address
+                    Navigation.findNavController(button_post_review).navigate(nextAction)
+                }
+                    .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
+                userRef.set(userFields, SetOptions.merge())
+            }
+            else{
+                var propertyFields = hashMapOf<Any, Any>()
+                val userFields = hashMapOf<Any, Any>()
+                val reviewer = hashMapOf<Any, Any>()
+                val property = hashMapOf<Any, Any>()
+
+                val reviewDetail = hashMapOf<Any,Any>(
+                    "comments" to comment.toString(),
+                    "isAnonymous" to anonymous_switch.isChecked,
+                    "isEdited" to false,
+                    "rating" to overall_rating.text.toString().toFloat(),
+                    "willLiveAgain" to live_here_again_switch.isChecked,
+                    "locationRating" to location_ratingBar.rating,
+                    "managementRating" to manage_ratingBar.rating,
+                    "amenitiesRating" to amenities_ratingBar.rating,
+                    "timeStamp" to FieldValue.serverTimestamp()
+                )
+
+                propertyFields["reviews"] = reviewer
+                reviewer[user!!.email!!] = reviewDetail
+
+                userFields["Review History"] = property
+                property[address] = reviewDetail
+
+
+                var avgAmR = 0.0
+                var avgMnR = 0.0
+                var avgLcR = 0.0
+                var avgOAR = 0.0
+                addressRef.get().addOnSuccessListener { document ->
+                    if (document != null) {
+                        var totalOverallRating = 0.0
+                        var totalAmentRating = 0.0
+                        var totalLocRating = 0.0
+                        var totalManageRating = 0.0
+
+                        var reviewerNum = 0
+
+                        val review = document.get("reviews") as HashMap<String, *>
+
+                        for (reviewer in review) {
+                            reviewerNum++
+                            val reviewMap = review
+                            val reviewInfo:HashMap<String,*>  = reviewMap.getValue(reviewer.key) as HashMap<String, *>
+                            totalLocRating += (reviewInfo.getValue("locationRating")).toString().toFloat()
+                            totalAmentRating += (reviewInfo.getValue("amenitiesRating")).toString().toFloat()
+                            totalManageRating += (reviewInfo.getValue("managementRating")).toString().toFloat()
+
+                            totalOverallRating += (reviewInfo.getValue("rating")).toString().toFloat()
+                        }
+                        avgAmR = totalAmentRating/reviewerNum
+                        avgMnR = totalManageRating/reviewerNum
+                        avgLcR = totalLocRating/reviewerNum
+
+                        avgOAR = totalOverallRating/reviewerNum
+                    }
+                }
+
+                propertyFields = hashMapOf(
+                    "avgAmenities" to avgAmR,
+                    "avgLocation" to avgLcR,
+                    "avgManage" to avgMnR,
+                    "avgOverallRating" to avgOAR
+                )
+
+                addressRef.set(propertyFields, SetOptions.merge()).addOnSuccessListener {
+                    Log.d(TAG, "DocumentSnapshot successfully written!")
+                    var nextAction = writeReviewFragmentDirections.actionWriteReviewFragment2ToDisplayListingFragment2()
+                    nextAction.dynamicAddress = address
+                    Navigation.findNavController(button_post_review).navigate(nextAction)
+                }
+                    .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+
+                userRef.set(userFields, SetOptions.merge())
+            }
+        }
     }
 
     private fun checkReview() {
@@ -73,6 +253,7 @@ class writeReviewFragment : Fragment() {
 
                         for (reviewer in review) {
                             if (reviewer.key == user.email){
+                                edited = true
                                 val reviewMap = review
                                 val reviewInfo:HashMap<String,*>  = reviewMap.getValue(reviewer.key) as HashMap<String, *>
                                 Toast.makeText(context!!, "You have already reviewed this property.", Toast.LENGTH_SHORT).show()
@@ -82,7 +263,7 @@ class writeReviewFragment : Fragment() {
                                 val ratingInt = (reviewInfo.getValue("rating"))
                                 seekBar.progress = ratingInt.toString().toDouble().roundToInt()
 
-                                overall_rating.text = (reviewInfo.getValue("rating")).toString()
+                                overall_rating.text = seekBar.progress.toString()
                                 location_ratingBar.rating = (reviewInfo.getValue("locationRating")).toString().toFloat()
                                 manage_ratingBar.rating = (reviewInfo.getValue("managementRating")).toString().toFloat()
                                 amenities_ratingBar.rating = (reviewInfo.getValue("amenitiesRating")).toString().toFloat()
