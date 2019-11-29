76f046facce63b17fe99b0534ba31c3c254cc4eb
dan-codes <dli123@syr.edu>
2019-11-29 18:51:06 -0500

    admin view UI

--- app/src/main/java/com/example/cribb/AdminView.kt
+++ app/src/main/java/com/example/cribb/AdminView.kt
@@ -1,12 +1,28 @@
 package com.example.cribb
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import androidx.lifecycle.Observer
+import com.example.cribb.data.App
 
 class AdminView : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_admin_view)
+
+        (application as App).preferenceRepository
+            .nightModeLive.observe(this, Observer { nightMode ->
+            nightMode?.let { delegate.localNightMode = it }
+        }
+        )
+    }
+
+    override fun onCreateView(name: String, context: Context, attrs: AttributeSet): View? {
+        return super.onCreateView(name, context, attrs)
+
     }
 }
